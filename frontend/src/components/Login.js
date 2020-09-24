391c6f8cd1e1537bbb4fce4ae022cb27b56b1747
s-katte <shubhamkatte99@gmail.com>
2020-09-24 20:39:30 +0530

    generating unique Ids on click

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useRef } from "react";
 import { Container, Form, Button } from "react-bootstrap";
+import { v4 as uuidv4 } from "uuid";
 
 function Login({ onIdSubmit }) {
   const idRef = useRef();
@@ -10,6 +11,9 @@ function Login({ onIdSubmit }) {
     onIdSubmit(idRef.current.value);
   }
 
+  function createNewId() {
+    onIdSubmit(uuidv4());
+  }
   return (
     <Container
       className="align-items-center d-flex"
@@ -23,7 +27,9 @@ function Login({ onIdSubmit }) {
         <Button type="submit" className="mr-2">
           Login
         </Button>
-        <Button variant="secondary">Create a new Id</Button>
+        <Button onClick={createNewId} variant="secondary">
+          Create a new Id
+        </Button>
       </Form>
     </Container>
   );
